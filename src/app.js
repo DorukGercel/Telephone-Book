85f622e09b05a3ffc1adf0698cfafea7a82b4e4d
DorukGercel <dorukgercel@hotmail.com>
2021-12-11 19:36:57 +0300

    Modify app

--- src/app.js
+++ src/app.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const app = express();
-const config = require("./config");
+const {PersonRoutes} = require("./routes");
+const config = require("./configs");
 const load = require("./loaders");
 
 config();
 load();
 
+app.use(express.json());
+
 app.listen(process.env.APP_PORT, () => {
     console.log(`Currently listening port ${process.env.APP_PORT}`);
+    app.use("/person", PersonRoutes);
 })
